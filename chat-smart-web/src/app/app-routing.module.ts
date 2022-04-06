42d25af0916034a3a2c8fd43b6abe17b71cefe62
David Sanchez <asturies@gmail.com>
2022-04-06 16:53:35 +0200

    Add guard to make sure all pages are accessed after logging in.

--- chat-smart-web/src/app/app-routing.module.ts
+++ chat-smart-web/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './authentication.guard';
+import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthenticationGuard] },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CoreModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
